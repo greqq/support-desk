f88728b4f33cf536d03f1a36c3532eb6066a337e
Luka Masanovic <lukamasanovic@gmail.com>
2022-03-22 14:09:18 +0100

    added frontend

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from headers
+      token = req.headers.authorization.split(' ')[1];
+      //verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Get user from token
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next();
+    } catch (error) {
+      console.log(error);
+      req.status(401);
+      throw new Error('Not authorized');
+    }
+  }
+
+  if (!token) {
+    req.status(401);
+    throw new Error('Not authorized');
+  }
+});
+
+module.exports = protect;
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const colors = require('colors');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+const PORT = process.env.PORT || 8000;
+
+//connect to DB
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log('server started'));
